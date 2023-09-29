7116acadf44bdb14c4a926c2fe1d77ddb7288fd1
Kadin Tanner <kadin.tanner@gmail.com>
2023-09-29 15:57:51 -0600

    assesment turn in at 3:30

--- src/arrow-fns.js
+++ src/arrow-fns.js
@@ -5,7 +5,9 @@
 // Ex.:
 //   giveMeTwo();
 //   => 2
-const giveMeTwo = undefined;
+const giveMeTwo = undefined => {
+    return 2
+}
 
 // Set the value of addNums to an arrow function that takes in two numbers and
 // returns their sum.
@@ -13,7 +15,9 @@ const giveMeTwo = undefined;
 // Ex.:
 //   addNums(1, 2);
 //   => 3
-const addNums = undefined;
+const addNums = (num1, num2) => {
+    return num1 + num2
+}
 
 // Set the value of max to an arrow function that takes in two numbers and
 // returns the largest one.
@@ -21,7 +25,9 @@ const addNums = undefined;
 // Ex.:
 //   max(1, 2);
 //   => 2
-const max = undefined;
+const max = (num1, num2) => {
+    if (num1 <= num2) return num2; else return false
+    }
 // Note: you can also do this on one line:
 // const max = (a, b) => (a >= b ? a : b);
 
@@ -31,7 +37,14 @@ const max = undefined;
 // Ex.:
 //   evens([1, 2, 3, 4, 5]);
 //   => [2, 4]
-function evens(nums) {}
+function evens(nums) {
+   let newArrOfEvens = nums % 2 === 0;
+   return nums.filter(newArrOfEvens)
+}
+
+// const evensNumbsOnly = (nums) => num % 2 === 0; {
+//     const evenNumbs = nums.filter(evens); return evenNumbs
+// }
 
 // Given an array of names, return a new array of greetings for each name. Do
 // this using array iteration methods. Do NOT use a for loop.
@@ -39,7 +52,12 @@ function evens(nums) {}
 // Ex.:
 //   createGreetings(['Clive', 'Jill', 'Torgal']);
 //   => ['Hello, Clive!', 'Hello, Jill!', 'Hello, Torgal!']
-function createGreetings(names) {}
+let names = ['Clive', 'Jill', 'Torgal']
+let newNamesArr = names.map(createGreetings)
+
+function createGreetings(names) {
+    return 'Hello, ' + names + '!'
+}
 
 // Given an array of words, return a new array where each word is uppercased
 // and only includes words that are longer than 4 characters. Do this using
@@ -49,5 +67,7 @@ function createGreetings(names) {}
 //   loudLongWords(['apple', 'pear', 'cake', 'pinata']);
 //   => ['APPLE', 'PINATA']
 function loudLongWords(words) {}
+
+// const loudLongWords = (words) => words.toUpperCase() + words.length > 4
 
 export { addNums, createGreetings, evens, giveMeTwo, loudLongWords, max };
